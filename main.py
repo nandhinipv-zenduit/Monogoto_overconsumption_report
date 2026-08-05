99d29ec0067196d899c4f365a2d8852132a6ef2a
nandhinipv-zenduit <nandhinipv@zenduit.com>
2026-08-05 15:57:12 +0530

    Update main.py

--- main.py
+++ main.py
@@ -81,7 +81,7 @@ EMAIL_SENDER = os.getenv("GMAIL_USERNAME")
 EMAIL_PASSWORD = os.getenv("GMAIL_PASS")
 EMAIL_TO = [
     "nandhinipv@zenduit.com", "nikithavinod@zenduit.com", "abidali@gofleet.com",
-    "adrianarosas@zenduit.com", "vanibukelia@zenduit.com", "rizamae@gofleet.com",
+     "vanibukelia@zenduit.com", "rizamae@gofleet.com",
 ]
 if not EMAIL_SENDER or not EMAIL_PASSWORD:
     raise RuntimeError("❌ Gmail credentials not found in environment variables")
